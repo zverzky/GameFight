e4f6ad09be45275f087b9f4d4b4b1b32fea0cfb3
Zverzky <Zverzky@DESKTOP-PHFT8G5>
2025-06-30 16:55:29 +0300

    Конечный вариант простой игры

---
+++ GameFight/Battle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFight
+{
+    public class Battle
+    {
+        private Character character1;
+        private Character character2;
+
+        public Battle(Character char1, Character char2)
+        {
+            character1 = char1;
+            character2 = char2;
+        }
+        public void StartBattle()
+        {
+            Console.WriteLine($"Бой начинается между {character1.Name} и {character2.Name}");
+            while ((character1.Health > 0) || (character2.Health > 0))
+            {
+                character1.Attack(character2);
+                if (character2.Health <= 0) break;
+                character2.Attack(character1);
+                if (character1.Health <= 0) break;
+            }
+            if (character1.Health <= 0 && character2.Health <= 0)
+            {
+                Console.WriteLine("Оба персонажа пали в бою!");
+            }
+            else if (character1.Health <= 0)
+            {
+                Console.WriteLine($"{character2.Name} победил!");
+            }
+            else
+            {
+                Console.WriteLine($"{character1.Name} победил!");
+            }
+        }
+    }
+}
---
+++ GameFight/main.cs
@@ -0,0 +1,13 @@
+﻿using GameFight;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Character rogue = new Rogue("Летучий", 20, 100, 100);
+        Character warrior = new Warrior("Воин", 15, 80, 80);
+
+        Battle battle = new Battle(warrior, rogue);
+        battle.StartBattle();
+    }
+}
